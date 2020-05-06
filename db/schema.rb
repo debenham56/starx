bd4d8cffa89a79c4eebd9b54f5de519fc025f17f
Roman Granskyi <camillabenini@protonmail.com>
2020-05-06 11:35:51 -0400

    Initial Commit

---
+++ db/schema.rb
@@ -0,0 +1,89 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_05_06_145543) do
+
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string "namespace"
+    t.text "body"
+    t.string "resource_type"
+    t.integer "resource_id"
+    t.string "author_type"
+    t.integer "author_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+  end
+
+  create_table "admin_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "address"
+    t.string "city"
+    t.string "country"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.boolean "shipped", default: false
+  end
+
+  create_table "product_items", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "cart_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "quantity", default: 1
+    t.integer "order_id"
+    t.index ["cart_id"], name: "index_product_items_on_cart_id"
+    t.index ["order_id"], name: "index_product_items_on_order_id"
+    t.index ["product_id"], name: "index_product_items_on_product_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.string "image"
+    t.text "description"
+    t.decimal "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "product_items", "carts"
+  add_foreign_key "product_items", "orders"
+  add_foreign_key "product_items", "products"
+end
---
+++ vendor/bundle/ruby/2.6.0/gems/braintree-2.101.0/lib/braintree/payment_instrument_type.rb
@@ -0,0 +1,17 @@
+module Braintree
+  module PaymentInstrumentType
+    PayPalAccount = 'paypal_account'
+    EuropeBankAccount = 'europe_bank_account'
+    CreditCard = 'credit_card'
+    CoinbaseAccount = 'coinbase_account'
+    ApplePayCard = 'apple_pay_card'
+    AndroidPayCard = 'android_pay_card'
+    VenmoAccount = 'venmo_account'
+    UsBankAccount = 'us_bank_account'
+    VisaCheckoutCard = 'visa_checkout_card'
+    MasterpassCard = 'masterpass_card'
+    SamsungPayCard = 'samsung_pay_card'
+    LocalPayment = 'local_payment'
+    PayPalHere = 'paypal_here'
+  end
+end
